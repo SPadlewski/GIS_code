b96c6db89af056ac2c11fe11d87154789df85bc9
SPadlewski <zcftpad@ucl.ac.uk>
2020-11-06 12:39:03 +0000

    part2

--- wk5/prac5_scripts/part1.R
+++ wk5/prac5_scripts/part1.R
@@ -4,6 +4,7 @@ library(tmap)
 library(tmaptools)
 library(tidyverse)
 library(here)
+library(grid)
 
 # read in all the spatial data and 
 # reproject it 
@@ -44,8 +45,11 @@ Airbnb <- read_csv("prac5_data/listings.csv") %>%
   filter(room_type == 'Entire home/apt' & availability_365 =='365')
 
 Rightmove <- read_csv("prac5_data/rightmove_05-11-2020.csv") %>% 
-  st_as_sf(.,coords= c("longitude","latitude"), crs = 4326) %>%
-  st_transform(., 27700)
+  #st_as_sf(.,coords= c("longitude","latitude"), crs = 4326) %>%
+  #st_transform(., 27700) 
+  
+
+plot(Rightmove["price_pcm"])
 
 # make a function for the join
 # functions are covered in practical 7
@@ -72,7 +76,7 @@ Hotels <- Joinfun(OSM, Londonborough)
 Airbnb <- Joinfun(Airbnb, Londonborough)
 
 # then for Rightmove
-Rightmove <- Joinfun(Rightmove,Londonborough)
+#Rightmove <- Joinfun(Rightmove,Londonborough)
 
 Worldcities2 <- Worldcities %>%
   filter(CNTRY_NAME=='United Kingdom'&
@@ -118,12 +122,12 @@ tm2 <- tm_shape(Airbnb) +
   tm_layout(frame=FALSE)+
   tm_credits("(b)", position=c(0,0.85), size=1.5)
 
-tm3 <- tm_shape(UK_Outlinecrop)+ 
+tm3 <- tm_shape(UK_outlinecrop)+ 
   tm_polygons(col="darkslategray1")+
   tm_layout(frame=FALSE)+
   tm_shape(Worldcities2) +
   tm_symbols(col = "red", scale = .5)+
-  tm_text("CITY_NAME", xmod=-1, ymod=-0.5)
+  tm_text("CITY_NAME", xmod=0, ymod=-1)
 
 legend <- tm_shape(Hotels) +
   tm_polygons("Accomodation count",
@@ -135,4 +139,49 @@ legend <- tm_shape(Hotels) +
 
 t=tmap_arrange(tm1, tm2, tm3, legend, ncol=2)
 
-plot(t)
+grid.newpage()
+
+pushViewport(viewport(layout=grid.layout(2,2)))
+print(tm1, vp=viewport(layout.pos.col=1, layout.pos.row=1, height=5))
+print(tm2, vp=viewport(layout.pos.col=2, layout.pos.row=1, height=5))
+print(tm3, vp=viewport(layout.pos.col=1, layout.pos.row=2, height=5))
+print(legend, vp=viewport(layout.pos.col=2, layout.pos.row=2, height=5))
+
+
+Londonbb = st_bbox(Airbnb,
+                   crs = st_crs(Airbnb)) %>% 
+  st_as_sfc()
+
+main <- tm_shape(Airbnb, bbbox = Londonbb) + 
+  tm_polygons("Accomodation count",
+              breaks=breaks, 
+              palette="PuBu")+
+  tm_scale_bar(position = c("left", "bottom"), text.size = .75)+
+  tm_layout(legend.position = c("right","top"), 
+            legend.text.size=.75, 
+            legend.title.size = 1.1,
+            frame=FALSE)+
+  tm_credits("(c) OpenStreetMap contrbutors and Air b n b", position=c(0.0,0.0))+
+  #tm_text(text = "NAME", size = .5, along.lines =T, remove.overlap=T,  auto.placement=F)+
+  tm_compass(type = "8star", position = c(0.06, 0.1)) +
+  
+  #bottom left top right
+  tm_layout(inner.margin=c(0.02,0.02,0.02,0.2))
+
+inset = tm_shape(UK_outlinecrop) + tm_polygons() +
+  tm_shape(Londonbb)+ 
+  tm_borders(col = "grey40", lwd = 3)+
+  tm_layout(frame=FALSE,
+            bg.color = "transparent")+
+  tm_shape(Worldcities2) +
+  tm_symbols(col = "red", scale = .5)+
+  tm_text("CITY_NAME", xmod=-1.5, ymod=-0.5)
+
+main
+print(inset, vp = viewport(0.86, 0.29, width = 0.5, height = 0.55))
+
+tmap_save(t, 'hotelsandairbnbR.png')
+
+
+tmap_save(main,insets_tm = inset,insets_vp=viewport(x=0.86, y=0.29, width=.5, height=.55), filename="test.pdf", dpi=600)
+
--- wk5/prac5_scripts/part2.R
+++ wk5/prac5_scripts/part2.R
@@ -4,19 +4,125 @@ library(tmap)
 library(tmaptools)
 library(tidyverse)
 library(here)
-
-
-#London Borough data is already in 277000
-Londonborough <- st_read(here::here("Prac5_data",
+library(dplyr)
+library(tidyr)
+library(raster)
+library(plotly)
+library(raster)
+library(ggplot2)
+library(gstat)
+  
+#loading data
+Londonward <- st_read(here::here("Prac5_data",
                                     "statistical-gis-boundaries-london", 
                                     "ESRI", 
-                                    "London_Borough_Excluding_MHW.shp"))%>%
+                                    "London_Ward.shp"))%>%
+  st_transform(., 27700)
+
+Camden <- Londonborough %>% 
+  filter(.,NAME=="Camden")
+
+#Cleaning data
+Rightmove_2 <- Rightmve %>% 
+  mutate(price_pcm=as.numeric(gsub('pcm|£|,','',price_pcm)))%>% 
+  drop_na(price_pcm)
+
+One_bed <- Rightmove_2 %>% 
+  filter(title=="1 bedroom apartment" | title=="1 bedroom flat")
+
+#trimming outliers
+One_bed <- One_bed %>% 
+  filter(price_pcm> quantile(price_pcm,0.25),
+         price_pcm< quantile(price_pcm,0.75))
+
+#Visualising data
+
+plot(Rightmove_2$price_pcm)
+
+median(One_bed$price_pcm)
+
+
+
+#Histogram
+
+ggplot(data=One_bed,mapping=aes(x=price_pcm))+
+  geom_histogram(color="black",
+                 fill="blue",
+                 binwidth = 50)+
+  geom_vline(aes(xintercept=median(price_pcm)),
+             color="red",
+             linetype="dashed",
+             size=1
+             )+
+  geom_vline(aes(xintercept=mean(price_pcm)),
+             color="green",
+             linetype="dashed",
+             size=1
+  )
+
+
+#geom_bar(mapping=aes(x=price_pcm))
+
+
+
+
+#Adding Geodata
+Rightmove_geo <- Rightmove_2 %>% 
+  st_as_sf(., coords = c("longitude", "latitude"), 
+           crs = 4326) %>%
+  st_transform(., 27700)
+
+One_bed_geo <- One_bed %>% 
+  st_as_sf(., coords = c("longitude", "latitude"), 
+           crs = 4326) %>%
   st_transform(., 27700)
 
 
-#rightmove data
-Rightmove_2 <- read_csv("prac5_data/rightmove_05-11-2020.csv",) %>% 
-  st_as_sf(.,coords= c("longitude","latitude"), crs = 4326) %>%
-  st_transform(., 27700) %>% 
-  
+plot(Rightmove_geo$geometry)
 
+#Interpolation
+
+plot(Camden$geometry)
+plot(st_geometry(One_bed_geo),add=TRUE)
+
+
+CamdenSP <- Camden %>% 
+  as(., 'Spatial')
+
+One_bed_geo_SP <- One_bed_geo %>% 
+  as(., 'Spatial') 
+
+emptygrd <- as.data.frame(spsample(CamdenSP, n=100000, type="regular", cellsize=10))
+
+names(emptygrd) <- c("X", "Y")
+
+coordinates(emptygrd) <- c("X", "Y")
+
+gridded(emptygrd) <- TRUE  # Create SpatialPixel object
+fullgrid(emptygrd) <- TRUE  # Create SpatialGrid object
+
+# Add the projection to the grid
+proj4string(emptygrd) <- proj4string(One_bed_geo_SP)
+
+
+# Interpolate the grid cells using a power value of 2 
+interpolate <- gstat::idw(price_pcm~1,One_bed_geo_SP, newdata=emptygrd, idp=2.0)
+
+# Convert output to raster object 
+ras <- raster(interpolate)
+# Clip the raster to Australia outline
+rasmask <- mask(ras, Camden)
+# Plot the raster
+plot(rasmask,add=TRUE)
+
+# Adding basemap OSM
+tmapcamden <- Camden %>%
+  st_bbox(.) %>% 
+  tmaptools::read_osm(., type = "osm", zoom = NULL)
+
+tmap_mode("plot")
+
+tm_shape(tmapcamden)+
+  tm_rgb()+
+  tm_shape(rasmask)+
+  tm_raster(palette = c("red","yellow"),alpha = 0.70)

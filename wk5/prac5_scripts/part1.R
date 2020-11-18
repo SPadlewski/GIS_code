0dc3b92991e7383d4d5857f0c9589400985f9a77
SPadlewski <zcftpad@ucl.ac.uk>
2020-11-18 13:13:44 +0000

    final

--- wk5/prac5_scripts/part1.R
+++ wk5/prac5_scripts/part1.R
@@ -185,3 +185,86 @@ tmap_save(t, 'hotelsandairbnbR.png')
 
 tmap_save(main,insets_tm = inset,insets_vp=viewport(x=0.86, y=0.29, width=.5, height=.55), filename="test.pdf", dpi=600)
 
+
+tmap_mode("view")
+
+tm_shape(Airbnb) + 
+  tm_polygons("Accomodation count", breaks=breaks) 
+
+# library for pop up boxes
+library(leafpop)
+library(leaflet)
+
+#join data
+Joined <- Airbnb%>%
+  st_join(., Hotels, join = st_equals)%>%
+  dplyr::select(GSS_CODE.x, NAME.x, `Accomodation count.x`, `Accomodation count.y`)%>%
+  dplyr::rename(`GSS code` =`GSS_CODE.x`,
+                `Borough` = `NAME.x`,
+                `Airbnb count` = `Accomodation count.x`,
+                `Hotel count`= `Accomodation count.y`)%>%
+  st_transform(., 4326)
+
+
+#remove the geometry for our pop up boxes to avoid
+popupairbnb <-Joined %>%
+  st_drop_geometry()%>%
+  dplyr::select(`Airbnb count`, Borough)%>%
+  popupTable()
+
+popuphotel <-Joined %>%
+  st_drop_geometry()%>%
+  dplyr::select(`Hotel count`, Borough)%>%
+  popupTable()
+
+tmap_mode("view")
+
+# set the colour palettes using our previously defined breaks
+
+
+pal1 <- Joined %>%
+  colorBin(palette = "YlOrRd", domain=.$`Airbnb count`, bins=breaks)
+
+pal1 <-colorBin(palette = "YlOrRd", domain=Joined$`Airbnb count`, bins=breaks)
+
+pal2 <- Joined %>%
+  colorBin(palette = "YlOrRd", domain=.$`Hotel count`, bins=breaks)
+
+
+map<- leaflet(Joined) %>%
+  # add basemap options
+  addTiles(group = "OSM (default)") %>%
+  addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+  addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+  addProviderTiles(providers$CartoDB.Positron, group = "CartoDB")%>%
+  
+  #add our polygons, linking to the tables we just made
+  addPolygons(color="white", 
+              weight = 2,
+              opacity = 1,
+              dashArray = "3",
+              popup = popupairbnb,
+              fillOpacity = 0.7,
+              fillColor = ~pal2(`Airbnb count`),
+              group = "Airbnb")%>%
+  
+  addPolygons(fillColor = ~pal2(`Hotel count`), 
+              weight = 2,
+              opacity = 1,
+              color = "white",
+              dashArray = "3",
+              popup = popupairbnb,
+              fillOpacity = 0.7,group = "Hotels")%>%
+  # add a legend
+  addLegend(pal = pal2, values = ~`Hotel count`, group = c("Airbnb","Hotel"), 
+            position ="bottomleft", title = "Accomodation count") %>%
+  # specify layers control
+  addLayersControl(
+    baseGroups = c("OSM (default)", "Toner", "Toner Lite", "CartoDB"),
+    overlayGroups = c("Airbnb", "Hotels"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+# plot the map
+map
+
